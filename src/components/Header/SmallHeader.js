8675aca8f864f0a0d13ffe7652d1e1c9a8bed689
allen3325 <00857146@email.ntou.edu.tw>
2022-01-14 16:42:27 +0800

    fix: calendar spell error.

--- src/components/Header/SmallHeader.js
+++ src/components/Header/SmallHeader.js
@@ -17,7 +17,7 @@ const SmallHeader = () => {
         <div>
             <ButtonGroup className="ButtonGroup" variant="text">
                 <MyThemeComponent href="/newDiary"><p className="smallButton">new diary</p></MyThemeComponent>
-                <MyThemeComponent href="/calenderSearch"><p className="smallButton">calender</p></MyThemeComponent>
+                <MyThemeComponent href="/calenderSearch"><p className="smallButton">calendar</p></MyThemeComponent>
                 {/* <MyThemeComponent href="/folderPage"><p className="smallButton">folder</p></MyThemeComponent> */}
                 {/* <MyThemeComponent href="/test"><p className="smallButton">test</p></MyThemeComponent> */}
                 {/* <MyThemeComponent href="/">outline page</MyThemeComponent> */}
