4d6e43462de397399e457105adb0189a62d0f9a3
yiting <00853129@email.ntou.edu.tw>
2022-01-18 22:41:51 +0800

    "fix:登入後 LoginPage redirect to HomePage"

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,14 +1,14 @@
 import "./LoginPage.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Container, Paper } from "@mui/material";
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import axios from "../axios/axios";
-import { Alert } from '@mui/material';
-import { Snackbar } from '@mui/material';
+import { Alert } from "@mui/material";
+import { Snackbar } from "@mui/material";
 import CookieParser from "../CookieParser/CookieParser";
 import { Navigate } from "react-router-dom";
 import LogInOrOutButton from "../Header/LogInOrOutButton";
@@ -18,42 +18,57 @@ function LoginPage() {
   const [openSuccess, setOpenSuccess] = React.useState(false);
   const [redirect, setRedirect] = React.useState(false);
   const [redirectActivate, setRedirectActivate] = React.useState(false);
+  const [shouldRedirect, setShouldRedirect] = React.useState(false);
   const [render, setRender] = React.useState(false);
   // let email = "";
   const [email, setEmail] = React.useState("");
   // let password = "";
   const [password, setPassword] = React.useState("");
+  
+  useEffect(() => {
+    let cookieParser = new CookieParser(document.cookie);
+    if (
+      cookieParser.getCookieByName("token") == "undefined" ||
+      cookieParser.getCookieByName("token") == null
+    ) {
+      setShouldRedirect(false);
+    } else {
+      setShouldRedirect(true);
+    }
+  }, []);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
     console.log(email);
-  }
+  };
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
     console.log(password);
-  }
+  };
 
   const login = () => {
+    let temp_email = email ? email : document.getElementById("email").value;
+    let temp_password = password
+      ? password
+      : document.getElementById("password").value;
 
-    let temp_email = (email) ? email : document.getElementById("email").value;
-    let temp_password = (password) ? password : document.getElementById("password").value;
-
-    axios.post("/login", {
-      email: temp_email,
-      password: temp_password
-    })
-      .then(res => {
+    axios
+      .post("/login", {
+        email: temp_email,
+        password: temp_password,
+      })
+      .then((res) => {
         document.cookie = "token=" + res.data.token;
         //console.log(document.cookie);
         let cookieParser_token = new CookieParser(document.cookie);
         document.cookie = "email=" + res.data.email;
         //console.log(document.cookie);
         let cookieParser_email = new CookieParser(document.cookie);
-        console.log(cookieParser_email.getCookieByName('email'));
+        console.log(cookieParser_email.getCookieByName("email"));
         // console.log(document.cookie);
         // console.log("success");
         // console.log(res);
-        <LogInOrOutButton />
+        <LogInOrOutButton />;
         console.log("parse");
         // console.log(cookieParser.getCookieByName('token'));
         setOpenSuccess(true);
@@ -62,23 +77,23 @@ function LoginPage() {
         setRedirect(true);
       })
       .catch((error) => {
-        console.log(error.response.status)
+        console.log(error.response.status);
         if (error.response.status === 403) {
           document.cookie = "email=" + temp_email;
           setRedirectActivate(true);
         }
         setOpenFail(true);
-      })
-  }
+      });
+  };
   const handleCloseFail = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     setOpenFail(false);
   };
 
   const handleCloseSuccess = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     setOpenSuccess(false);
@@ -95,7 +110,9 @@ function LoginPage() {
   //   });
   // };
 
-  return (
+  return shouldRedirect ? (
+    <Navigate to={`/`} />
+  ) : (
     <Container maxWidth={"sm"}>
       <Paper elevation={0} style={{ height: "100vh" }} className="login_page">
         {redirectActivate ? <Navigate to={"/activate"} /> : ""}
@@ -104,9 +121,9 @@ function LoginPage() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
           <Typography component="h1" variant="h5">
@@ -151,12 +168,20 @@ function LoginPage() {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link sx={{ fontSize: "1rem", color: "#818ea3" }} href="/forgotpassword" variant="body2">
+              <Link
+                sx={{ fontSize: "1rem", color: "#818ea3" }}
+                href="/forgotpassword"
+                variant="body2"
+              >
                 Forgot password?
               </Link>
             </Grid>
             <Grid item>
-              <Link sx={{ fontSize: "1rem", color: "#818ea3" }} href="/register" variant="body2">
+              <Link
+                sx={{ fontSize: "1rem", color: "#818ea3" }}
+                href="/register"
+                variant="body2"
+              >
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
@@ -164,13 +189,29 @@ function LoginPage() {
           {/* </Box> */}
         </Box>
         {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
-        <Snackbar open={openFail} autoHideDuration={2000} onClose={handleCloseFail}>
-          <Alert onClose={handleCloseFail} severity="error" sx={{ width: '100%' }}>
+        <Snackbar
+          open={openFail}
+          autoHideDuration={2000}
+          onClose={handleCloseFail}
+        >
+          <Alert
+            onClose={handleCloseFail}
+            severity="error"
+            sx={{ width: "100%" }}
+          >
             error login informantion!!
           </Alert>
         </Snackbar>
-        <Snackbar open={openSuccess} autoHideDuration={2000} onClose={handleCloseSuccess}>
-          <Alert onClose={handleCloseSuccess} severity="success" sx={{ width: '100%' }}>
+        <Snackbar
+          open={openSuccess}
+          autoHideDuration={2000}
+          onClose={handleCloseSuccess}
+        >
+          <Alert
+            onClose={handleCloseSuccess}
+            severity="success"
+            sx={{ width: "100%" }}
+          >
             login successfully.
           </Alert>
         </Snackbar>
