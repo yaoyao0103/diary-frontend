effde23f34ec6fae2bcbeef8a6fb6344263a6398
allen3325 <00857146@email.ntou.edu.tw>
2022-01-19 15:41:33 +0800

    fix: use heroku's URL.

--- src/components/axios/axios.js
+++ src/components/axios/axios.js
@@ -5,7 +5,7 @@ const axios = (baseURL) => {
     let cookieParser = new CookieParser(document.cookie);
     //建立一個自定義的axios
     const instance = _axios.create({
-        baseURL: baseURL || 'http://127.0.0.1:3001', //JSON-Server端口位置
+        baseURL: baseURL || 'https://diary-backend-app.herokuapp.com', //JSON-Server端口位置
         // baseURL: baseURL || 'http://192.168.0.7:3001', //JSON-Server端口位置
     });
 
